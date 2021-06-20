7c27bc6e1be8d7a3834ea9782a3d5dbf996ce523
RishiKetuChand <rishiketuchand64@gmail.com>
2021-06-20 17:18:36 +0530

    Splash Screen Animation Complete

--- app/src/main/java/com/example/toycathon/SplashActivity.java
+++ app/src/main/java/com/example/toycathon/SplashActivity.java
@@ -2,17 +2,46 @@ package com.example.toycathon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 import java.util.Objects;
 
 public class SplashActivity extends AppCompatActivity {
+    LottieAnimationView lottieAnimationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Objects.requireNonNull(getSupportActionBar()).hide();
+        lottieAnimationView=findViewById(R.id.animationView);
+        lottieAnimationView.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
 
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                Intent intent= new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
     }
 }
